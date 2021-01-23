1825ee50174db673b532add22605459a30fafc70
mursalin117 <mursalinchowdhury117@gmail.com>
2021-01-24 00:28:52 +0600

    initial commit

--- Basic PHP/operators.php
+++ Basic PHP/operators.php
@@ -65,6 +65,69 @@
     echo "The values of x and y are not same: ";
     var_dump($x <> $y);
     echo "<br>";
+    // not identical - checks both the values and types of x and y
+    echo "The x and y are not identical: ";
+    var_dump($x !== $y);
+    echo "<br>";
+    // greather than - works for both same type and different type
+    $x = 100;
+    $y = "50";
+    echo "x is greater than y: ";
+    var_dump($x > $y);
+    echo "<br>";
+    // less than - works for both same type and different type
+    echo "y is less than x: ";
+    var_dump($y < $x);
+    echo "<br>";
+    // greater than or equal - works for both same type and different type
+    echo "x is greater than or equal y: ";
+    var_dump($x >= $y);
+    echo "<br>";
+    // less than or equal - works for both same type and different type
+    echo "x is less than or equal y: ";
+    var_dump($x <= $y);
+    echo "<br>";
+    // spaceship - checks if the value is greater than or equal or less than both at the same time
+    // for greater than
+    $x = 100; // works both for same type and different type
+    $y = "50";
+    echo "x is greater than y: ";
+    var_dump($x <=> $y);
+    echo "<br>";
+    // for less than
+    $x = 10;
+    $y = 50;
+    echo "x is less than y: ";
+    var_dump($x <=> $y);
+    echo "<br>";
+    // for equal
+    $x = 100;
+    $y = 100;
+    echo "x is equal to y: ";
+    var_dump($x <= $y);
+    echo "<br>";
+    echo "<hr>";
+
+    // increment/decrement operators
+    // pre-increment operators - increment the value instantly
+    $x = 10;
+    echo "Before pre-increment x is: " . $x . "<br>";
+    echo "After pre-increment x is: " . ++$x . "<br>";
+    // post-increment - increment value after the execution
+    $x = 10;
+    echo "Before post-increment x is: " . $x . "<br>";
+    echo "After post-increment x is: " . $x++ . "<br>";
+    echo "Now x is: " . $x . "<br>";
+    // pre-decrement - same as pre-increment
+    $x = 10;
+    echo "Before pre-decrement x is: " . $x . "<br>";
+    echo "After pre-decrement x is: " . --$x . "<br>";
+    // post-decrement - same as post-increment
+    $x = 10;
+    echo "Before post-decrement x is: " . $x . "<br>";
+    echo "After post-decrement x is: " . $x-- . "<br>";
+    echo "Now x is: " . $x . "<br>";
+    echo "<hr>";
 
     ?>
 
